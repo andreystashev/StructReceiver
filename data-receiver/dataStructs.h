3dd68b130d9cf1bf57023445223ab9605f483ff1
Andrey <70208784+andreystashev@users.noreply.github.com>
2024-06-19 19:21:27 +0300

    edit tasks

--- data-receiver/dataStructs.h
+++ data-receiver/dataStructs.h
@@ -13,11 +13,11 @@ struct DataStruct
         unsigned int word; // слово данных
         struct {
             unsigned char addr : 8;     // адрес (в 16-тиричном представлении)
-            unsigned char reserve1 : 6; // какой-то резерв, не нужно для использования
+            unsigned char reserve1 : 6; // резерв
             bool dataOk : 1;            // признак корректности данных
             bool onLand : 1;            // признак нахождения на земле
             unsigned int speed : 10;    // скорость, км/ч (цена старшего разряда - 512)
-            unsigned char reserve2 : 3; // какой-то резерв, не нужно для использования
+            unsigned char reserve2 : 3; // резерв
             unsigned char matrix : 2;   // матрица состояния(00 - отказ, 01 – нет данных, 10 – тест, 11 - нормальная работа)
             bool ctrlBit : 1;           // бит контроля чётности
         } data;
